36120fe2f8a447047e63432e4f46d41e0f01128a
Yan Shabalin <yanshabalin@gmail.com>
2023-04-08 00:09:21 +0700

    added arguments and new option for gendiff command

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,4 +4,7 @@ import { program } from 'commander';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f', '--format <type>',  'output format')
   .parse(process.argv);
