16da6785b23b4a780e028511036867da21c42ae6
Yan Shabalin <yanshabalin@gmail.com>
2023-04-12 20:44:46 +0700

    added plain gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-
+import gendiff from '../src/gendiff.js';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .option('-f', '--format <type>',  'output format')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2))
+  })
   .parse(process.argv);
