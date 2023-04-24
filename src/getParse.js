0ace094facef0130aad6f128bcb3cb91188c3e43
Yan Shabalin <yanshabalin@gmail.com>
2023-04-25 02:43:26 +0700

    made fix lint

--- src/getParse.js
+++ src/getParse.js
@@ -1,15 +1,15 @@
 import yaml from 'js-yaml';
 
 const getParse = (data, format) => {
-  switch (format) {
-    case '.json':
-      return JSON.parse(data);
-    case '.yml':
-    case '.yaml':
-      return yaml.load(data);
-    default:
-      throw new Error(`Format ${format} is not supported`);
-  }
+	switch (format) {
+	case '.json':
+		return JSON.parse(data);
+	case '.yml':
+	case '.yaml':
+		return yaml.load(data);
+	default:
+		throw new Error(`Format ${format} is not supported`);
+	}
 };
 
 export default getParse;
