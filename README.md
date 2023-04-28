3fb01b8ce74e170442193ad2d2414ccdeee3cccb
Yan Shabalin <yanshabalin@gmail.com>
2023-04-28 16:48:11 +0700

    attempt to improve code №1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-###tests and linter status:
+### tests and linter status:
 [![Actions Status](https://github.com/slowyan/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/slowyan/frontend-project-46/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/41d8e73542c2d0bc71b8/maintainability)](https://codeclimate.com/github/slowyan/frontend-project-46/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/41d8e73542c2d0bc71b8/test_coverage)](https://codeclimate.com/github/slowyan/frontend-project-46/test_coverage)
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,11 +1,11 @@
 import generationFormat from '../formatters/generationFormat.js';
 import buildTree from './buildTree.js';
 import getParse from './getParse.js';
-import readFile from './readFile.js';
 import path from 'path';
+import fs from 'fs';
 
 const gendiff = (filepath1, filepath2, format = 'stylish') => {
-	const [obj1, obj2] = [filepath1, filepath2].map((fp) => getParse(readFile(fp), path.extname(fp)));
+	const [obj1, obj2] = [filepath1, filepath2].map((fp) => getParse(fs.readFileSync(fp), path.extname(fp)));
 	const diffTree = buildTree(obj1, obj2);
 	return generationFormat(diffTree, format);
 };
