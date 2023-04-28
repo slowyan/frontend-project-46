f27440cc4507f8b4b0b9defb7314ab0715971520
Yan Shabalin <yanshabalin@gmail.com>
2023-04-28 18:25:23 +0700

    2 war with workflow

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,9 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { resolve } from 'node:path';
+import { cwd } from 'node:process';
 import gendiff from '../src/gendiff.js';
 import fs from 'fs';
-import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filepath) => resolve(cwd(), '__fixtures__', filepath);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('gendiff json nested', () => {
