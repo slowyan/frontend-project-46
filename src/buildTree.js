2528f1cee22facc03daed60ed4d7f4cb732034cb
Yan Shabalin <yanshabalin@gmail.com>
2023-05-08 05:01:31 +0700

    removed mutating sort

--- src/buildTree.js
+++ src/buildTree.js
@@ -4,7 +4,7 @@ const buildTree = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const unionKeys = _.union(keys1, keys2);
-  const sortedKeys = unionKeys.sort();
+  const sortedKeys = _.sortBy(unionKeys);
   const diff = sortedKeys.map((key) => {
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
       return { key, type: 'nested', children: buildTree(obj1[key], obj2[key]) };
